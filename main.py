74cfd705bcb0481b1169d2101f8b6e1c7e7a979d
artiom.danilov@outlook.com <artiom.danilov@outlook.com>
2021-10-24 23:24:32 +0400

    Lab2 almost done. Need to do docstrings

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
 import json
 import os
+from tqdm import tqdm
+import argparse
 
 
 class ReadFile:
@@ -101,24 +103,42 @@ class Validator:
             return 0
 
 
+parser = argparse.ArgumentParser(description='main.py')
+input_filename = ""
+output_filename = ""
+parser.add_argument('-i', '--input', type=str, help='Путь к файлу, откуда считать данные', required=True,
+                    dest='inputfilename')
+parser.add_argument('-o', '--output', type=str, help='Путь к файлу, куда записать валидные данные', required=True,
+                    dest='outputfilename')
+args = parser.parse_args()
+input_filename = os.path.realpath(args.inputfilename)
+output_filename = os.path.realpath(args.outputfilename)
 counter = 0
 codes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-rfile = ReadFile("63.txt")
-wfile = open("result63.txt", 'w')
-for elem in rfile.data:
-    counter += 1
-    check = Validator(elem['email'], elem['height'], elem['inn'], elem['passport_number'], elem['occupation'],
-                      elem['work_experience'], elem['academic_degree'], elem['worldview'], elem['address'])
-    code = check.check_all()
-    codes[code] += 1
+rfile = ReadFile(input_filename)
+wfile = open(output_filename, 'w')
+with tqdm(rfile.data, desc='Проверка записей') as progressbar:
+    for elem in rfile.data:
+        counter += 1
+        check = Validator(elem['email'], elem['height'], elem['inn'], elem['passport_number'], elem['occupation'],
+                          elem['work_experience'], elem['academic_degree'], elem['worldview'], elem['address'])
+        code = check.check_all()
+        codes[code] += 1
 
-    if code == 0:
-        wfile.write("email: " + elem["email"] + "\n" + "height:" + str(elem["height"]) + "\n" +
-                    "inn: " + str(elem["inn"]) + "\n" + "passport_number:" + str(elem["passport_number"]) + "\n" +
-                    "occupation: " + elem["occupation"] + "\n" + "work_experience: " + str(elem["work_experience"]) +
-                    "\n" + "academic_degree: " + elem["academic_degree"] + "\n" + "worldview: " + elem["worldview"] +
-                    "\n" + "address: " + elem["address"] + "\n" + "__________________________________________\n")
+        if code == 0:
+            wfile.write("email: " + elem["email"] + "\n" + "height:" + str(elem["height"]) + "\n" +
+                        "inn: " + str(elem["inn"]) + "\n" + "passport_number:" + str(elem["passport_number"]) + "\n" +
+                        "occupation: " + elem["occupation"] + "\n" + "work_experience: " + str(elem["work_experience"])
+                        + "\n" + "academic_degree: " + elem["academic_degree"] + "\n" + "worldview: " + elem[
+                            "worldview"] +
+                        "\n" + "address: " + elem["address"] + "\n" + "__________________________________________\n")
+        progressbar.update(1)
 wfile.close()
 errors = codes[1] + codes[2] + codes[3] + codes[4] + codes[5] + codes[6] + codes[7] + codes[8] + codes[9]
-print("Количество ошибок из-за опредленных полей: \nВсего пользователей: " + str(
-    counter) + "\nЧисло валидных записей - " + str(codes[0]) + "\nЧисло невалидных записей - " + str(errors))
+print("Всего пользователей: " + str(counter) + "\nЧисло валидных записей - " + str(codes[0]) +
+      "\nЧисло невалидных записей - " + str(errors))
+print("Количество ошибок в поле email - ", codes[1], "\n", "Количество ошибок в поле height - ", codes[2], "\n",
+      "Количество ошибок в поле inn - ", codes[3], "\n", "Количество ошибок в поле passport_number - ", codes[4], "\n",
+      "Количество ошибок в поле occupation - ", codes[5], "\n", "Количество ошибок в поле work_experience - ", codes[6],
+      "\n", "Количество ошибок в поле academic_degree - ", codes[7], "\n", "Количество ошибок в поле worldview - ",
+      codes[8], "\n", "Количество ошибок в поле address - ", codes[9], "\n", sep='')
